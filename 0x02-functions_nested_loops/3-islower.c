dd520a15250316afd094daf307bd3cb5fcde4435
MOB-MKO <mukaramoladega@gmail.com>
2022-12-14 11:26:18 +0100

    working directory

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,15 +6,8 @@
  */
 int _islower(int c)
 {
-	int i = 'a';
-
-	for  (i = 'a'; i <= 'z'; i++)
-	{
-		/* refer int c*/
-		if (c == i)
-		{
-			return (1);
-		}
-	}
-	return (0);
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
 }
